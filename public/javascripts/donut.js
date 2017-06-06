20220f95c2bf903a0cf62553e81d36b5f4c53437
Freakmiko <freakmiki+smfgit@gmail.com>
2017-06-06 19:00:17 +0200

    Donut now shows example data

--- public/javascripts/donut.js
+++ public/javascripts/donut.js
@@ -1,17 +1,15 @@
 /// <reference path="../node_modules/@types/d3/index.d.ts" />
-var data = [
-    { value: 10 },
-    { value: 30 },
-    { value: 20 },
-];
-var svg = d3.select("#donut"), width = +svg.attr("width"), height = +svg.attr("height"), radius = Math.min(width, height) / 2, g = svg.append("g").attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")");
-var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-var pie = d3.pie().sort(null).value(function (data) { return data.value; });
-var path = d3.arc().outerRadius(radius - 10).innerRadius(radius - 30).padAngle(0.03);
-var arc = g.selectAll(".arc")
-    .data(pie(data))
-    .enter().append("g")
-    .attr("class", "arc");
-arc.append("path").attr("d", path)
-    .attr("fill", function (data) { return color(data.value); });
+d3.json("/data/korean_election_2017_de.json", function (error, d) {
+    var data = d.items;
+    var svg = d3.select("#donut"), width = +svg.attr("width"), height = +svg.attr("height"), radius = Math.min(width, height) / 2, g = svg.append("g").attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")");
+    var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    var pie = d3.pie().sort(null).value(function (data) { return data.views; });
+    var path = d3.arc().outerRadius(radius - 10).innerRadius(radius - 30).padAngle(0.01);
+    var arc = g.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+    arc.append("path").attr("d", path)
+        .attr("fill", function (data) { return color(data.views); });
+});
 //# sourceMappingURL=donut.js.map
