10aa5acf7624969dce4a157299d264af78624fb8
RicoZieger <rico.zieger@gmail.com>
2017-06-17 22:41:28 +0200

    Added candidate information sidebar

--- public/javascripts/selection_page.js
+++ public/javascripts/selection_page.js
@@ -1,3 +1,25 @@
+var candidatesInfoText = new Array ();
+candidatesInfoText['CLINTON'] = "Allgemein:Lorem ipsum dolor sit amet,consectetuer adipiscing elit,sed diam nonummy nibh euismod tincidunt ut laoreet, dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit cilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consec-";
+
+
+var candidatesInfoImage = new Array ();
+candidatesInfoImage['CLINTON'] = "/data/Clinton.jpg";
+
+var candidatesNames = new Array();
+candidatesNames['CLINTON'] = "Hillary Clinton";
+
+
 function back() {
 	window.open("http://localhost:3000/","_self")
+}
+
+function showCandidateInformation(name){		
+	document.getElementById("sidebarInfoContainer").style.visibility = "visible";	
+	document.getElementById("candidateName").textContent = candidatesNames[name];
+	document.getElementById("candidateInfoText").textContent = candidatesInfoText[name];
+	document.getElementById("candidateInfoImage").src=candidatesInfoImage[name];
+}
+
+function hideCandidateInformation(){
+	document.getElementById("sidebarInfoContainer").style.visibility = "hidden";	
 }
