b5dbca8435aec81d8fa9a561fabcfbee9bab9899
RicoZieger <rico.zieger@gmail.com>
2017-06-18 15:03:56 +0200

    *Added info text for the us and austrian elections *Added images of all us and austrian candidates

--- public/javascripts/selection_page.js
+++ public/javascripts/selection_page.js
@@ -1,12 +1,38 @@
 var candidatesInfoText = new Array ();
 candidatesInfoText['CLINTON'] = "Allgemein:Lorem ipsum dolor sit amet,consectetuer adipiscing elit,sed diam nonummy nibh euismod tincidunt ut laoreet, dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit cilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consec-";
-
+candidatesInfoText['TRUMP'] = "";
+candidatesInfoText['SANDERS'] = "";
+candidatesInfoText['CRUZ'] = "";
+candidatesInfoText['KASICH'] = "";
+candidatesInfoText['BELLEN'] = "";
+candidatesInfoText['HOFER'] = "";
+candidatesInfoText['GRISS'] = "";
+candidatesInfoText['HUNDSTORFER'] = "";
+candidatesInfoText['KHOL'] = "";
 
 var candidatesInfoImage = new Array ();
 candidatesInfoImage['CLINTON'] = "/data/Clinton.jpg";
+candidatesInfoImage['TRUMP'] = "/data/Trump.jpg";
+candidatesInfoImage['SANDERS'] = "/data/Sanders.jpg";
+candidatesInfoImage['CRUZ'] = "/data/Cruz.jpg";
+candidatesInfoImage['KASICH'] = "/data/Kasich.jpg";
+candidatesInfoImage['BELLEN'] = "/data/Bellen.jpg";
+candidatesInfoImage['HOFER'] = "/data/Hofer.jpg";
+candidatesInfoImage['GRISS'] = "/data/Griss.jpg";
+candidatesInfoImage['HUNDSTORFER'] = "/data/Hundstorfer.jpg";
+candidatesInfoImage['KHOL'] = "/data/Khol.jpg";
 
 var candidatesNames = new Array();
 candidatesNames['CLINTON'] = "Hillary Clinton";
+candidatesNames['TRUMP'] = "Donald Trump";
+candidatesNames['SANDERS'] = "Bernie Sanders";
+candidatesNames['CRUZ'] = "Ted Cruz";
+candidatesNames['KASICH'] = "John Kasich";
+candidatesNames['BELLEN'] = "Alexander Van der Bellen";
+candidatesNames['HOFER'] = "Norbert Hofer";
+candidatesNames['GRISS'] = "Irmgard Griss";
+candidatesNames['HUNDSTORFER'] = "Rudolf Hundstorfer";
+candidatesNames['KHOL'] = "Andreas Khol";
 
 
 function back() {
