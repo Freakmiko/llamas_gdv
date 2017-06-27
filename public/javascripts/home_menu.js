8bba23ac547ecafb47ba5067a540648f9fc998cf
RicoZieger <rico.zieger@gmail.com>
2017-06-28 01:25:56 +0200

    *Removed language comparison from home menu *Improved Hover Effect

--- public/javascripts/home_menu.js
+++ public/javascripts/home_menu.js
@@ -1,17 +1,41 @@
 function setFranceInfotext() {
     document.getElementById("infotext").textContent = "Die französische Präsidentschaftswahl 2017 war erst die elfte ihrer Art dieser fünften französischen Republik. Da Sie nur kurz nach dem Brexit-Votum stattfand, stand die französische Präsidentschaftswahl unter besondere Beobachtung, galt sie doch als Wahl über das Fortbestehen der Europäischn Union. Viele Experten prognostizierten ein Auseinanderbrechen der EU, sollte Le Pen, die einen europafeindlichen Kurs verfolgte, die Wahl gewinnen und Frankreich ebenfalls seinen Austritt aus der Union verkünden. Der erste Wahlgang fand am 23. April 2017 statt; die entscheidende Stichwahl zwischen Emmanuel Macron und Marine Le Pen am 7. Mai 2017. Macron gewann die Wahl deutlich mit 66,1 % der Stimmen.";
+	
+	var foo = document.getElementById("title");
+	foo.innerHTML = "Präsidentschaftswahlen";
+	var br = document.createElement("br");    
+    foo.appendChild(br);
+	foo.innerHTML = foo.innerHTML + "-Frankreich-";
 }
 
 function setAustriaInfotext() {
     document.getElementById("infotext").textContent = "Als die Österreicher am 24. April 2016 ihren Gang zur Wahlurne antraten, standen mit 6 möglichen Kandidaten so viele wie seit 1951 nicht mehr zur Wahl. Folglich konnte keiner der Kandidaten in der ersten Wahlrunde die erforderliche absolute Mehrheit erreichen. Die Stichwahl zwischen Norbert Hofer, der klassisch rechtsextreme Positionen vertritt und Alexander Van der Bellen, welcher in der politischen Mitte einzuordnen ist, verkam in Zeiten der Flüchtlingskrise zur Abstimmung über die politische Ausrichtung Österreichs. Nachdem die Stichwahl wegen Verstößen gegen das Wahlgesetz wiederholt werden musste, setzte sich in einer zweiten Stichwahl am 4. Dezember 2016 Alexander Van der Bellen letztlich gegen Norbert Hofer durch.";
+	
+	var foo = document.getElementById("title");
+	foo.innerHTML = "Präsidentschaftswahlen";
+	var br = document.createElement("br");    
+    foo.appendChild(br);
+	foo.innerHTML = foo.innerHTML + "-Österreich-";
 }
 
 function setUSAInfotext() {
     document.getElementById("infotext").textContent = "Am 8. November 2016 fanden die 58. Präsidentschaftswahlen der USA statt. Mit Hillary Clinton als Kandidatin der Demokraten trat erstmals eine Präsidentschaftskandidatin an. Für die Republikaner trat mit Donald Trump ein schwerreicher Unternehmer ohne politische Erfahrung an, dem von Anfang an nur geringe Chancen auf den Wahlgewinn zugestanden wurden. Die Besonderheit des amerikanischen Wahlsystems liegt darin, dass in fast allen Staaten der Kandidat mit den meisten Stimmen diesen Staat für sich gewinnt und die Stimmen aller Wahlmänner dieses Staats erhält. Dies wurde Donald Trump zum Vorteil, da dieser trotz insgesamt knapp 3 Millionen Stimmen weniger als Hillary Clinton die Wahl recht deutlich gewann.";
+		
+	var foo = document.getElementById("title");
+	foo.innerHTML = "Präsidentschaftswahlen";
+	var br = document.createElement("br");    
+    foo.appendChild(br);
+	foo.innerHTML = foo.innerHTML + "-USA-";
 }
 
 function setSouthKoreaInfotext() {
     document.getElementById("infotext").textContent = "Die 19. Präsidentschaftswahl in Südkorea fand am 9. Mai 2017 statt. Die vorgezogene Wahl wurde nötig, nachdem die vorherige Präsidentin Park Geun-hye aufgrund eines Korruptionsskandals am 10. März 2017 ihres Amtes enthoben worden war. Regulär sollte die Wahl erst im Dezember 2017 stattfinden. Die Entscheidung über das neue Staatsoberhaupt wurde durch eine Mehrheitswahl (d. h. ohne Stichwahl) in einem Durchgang gefällt. In Südkorea sind Präsidentschaften auf eine Amtszeit beschränkt. Laut dem offiziellen Endergebnis der südkoreanischen Wahlkommission gewann Moon Jae-in von der Minju-Partei mit 41,08 Prozent der Stimmen. Die Wahlbeteiligung betrug 77,2 Prozent.";
+		
+	var foo = document.getElementById("title");
+	foo.innerHTML = "Präsidentschaftswahlen";
+	var br = document.createElement("br");    
+    foo.appendChild(br);
+	foo.innerHTML = foo.innerHTML + "-Südkorea-";
 }
 
 function setAllInfotext() {
